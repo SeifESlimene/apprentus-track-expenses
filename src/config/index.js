6bcfe81f6cbdc32602dec4af1eafb329f83ff558
SeifESlimene <s.slimene19@gmail.com>
2020-12-16 12:04:15 +0100

    deploy

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 // Base URL For All Our API Requests
 const config = () => {
-  axios.defaults.baseURL = process.env.REACT_APP_BASE_URI;
+  axios.defaults.baseURL =
+    process.env.NODE_ENV === "developement"
+      ? process.env.REACT_APP_BASE_URI_DEVELOPEMENT
+      : process.env.REACT_APP_BASE_URI_PRODUCTION;
 };
 
 export default config;
